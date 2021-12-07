a29a9e7ce6d518ed11bcca192f10612bb368eb37
Jean-Christophe Chevrier <chevrjc@gmail.com>
2021-12-07 07:53:07 -0800

    Bases des scripts de notification.

---
+++ src/notifications/send_email.sh
@@ -0,0 +1,36 @@
+#!/boin/bash
+
+
+# Script d'envoi d'e-mail.
+
+
+# Foncion d'envoi d'e-mail.
+function send() {
+	# Paramètres.
+	recipients=$1
+	subject=$2
+	content=$3
+
+	# Envoi de l'email.
+	echo "$content" | mail "$subject" "$recipients"
+
+	# Résultat de l'envoi.
+	return $?
+}
+
+
+# Exécution.
+if [ $# -eq 3 ]
+then
+	send $@
+	if [ $? -eq 0 ]
+	then
+		exit 0
+	else
+		echo "Echec de l'envoi de l'e-mail !"
+		exit 1
+	fi
+else
+	echo "Destinataire(s) et/ou sujet et/ou contenu de l'e-mail non renseigné(s) !"
+	exit 1
+fi
--- src/notifications/send_sms.sh
+++ src/notifications/send_sms.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 
+
 # Script d'envoi de sms.
 
 
 # Envoyer un SMS.
 function send() {
+	#Paramètres.
 	phoneNumber=$1
 	SMS=$2
+
+	# Envoi du sms.
 	result=$(curl -X POST https://textbelt.com/text \
-	--data-urlencode phone="$phoneNumbers" \
+	--data-urlencode phone="$phoneNumber" \
 	--data-urlencode message="$SMS" \
-	-d key=textbelt)
-	# TODO analyser le resultat et envoyer le bon code de retour
+	-d key=textbelt --silent)
+
+	# Résultat de l'envoi.
+	if [ $? -eq 1 ]
+	then
+		return 1
+	else
+		return $(echo $result | grep -c -E "{\"success\":false,")
+	fi
 }
 
 
 #Exécution.
 if [ $# -eq 2 ]
 then
-	send
-	exit 0
+	send $@
+	if [ $? -eq 0 ]
+	then
+		exit 0
+	else
+		echo "Echec de l'envoi du SMS !"
+		exit 1
+	fi
 else
-	echo "Numéro de téléphone, et contenu du SMS non renseigné."
+	echo "Numéro de téléphone et/ou contenu du SMS non renseigné(s) !"
 	exit 1
 fi
